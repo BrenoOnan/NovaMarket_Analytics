b92a8769f0f2f9b2f04342ce121f8291bf15ed32
Breno Camargo <211529127+BrenoOnan@users.noreply.github.com>
2026-08-30 14:27:08 -0300

    tabela purchase_history
    - Adiciona a tabela purchase_history para acompanhar o ciclo de vida do cliente e métricas de CRM.
    - Inclui métricas: primeira/última compra, total de pedidos, valor acumulado, ticket médio e status de fidelização.
    - Define a CHAVE PRIMÁRIA em cliente_id_history com CHAVE ESTRANGEIRA vinculada a clients(client_id).
    - Configura regras de CASCADE para exclusão e atualização para garantir a integridade dos dados.

--- database/create_table.sql
+++ database/create_table.sql
@@ -16,6 +16,8 @@ ENGINE=InnoDB DEFAULT
 CHARSET=utf8mb4 
 COLLATE=utf8mb4_0900_ai_ci;
 
+
+
 # Criando a tabela clientes
 
 CREATE TABLE novamarket.clients (
@@ -28,6 +30,24 @@ CREATE TABLE novamarket.clients (
 	region varchar(50) NOT NULL,
 	CONSTRAINT clients_pk PRIMARY KEY (client_id),
 	CONSTRAINT clients_unique UNIQUE KEY (email)
+)
+ENGINE=InnoDB
+DEFAULT CHARSET=utf8mb4
+COLLATE=utf8mb4_0900_ai_ci;
+
+
+
+# Criando a tabela historico compra
+CREATE TABLE novamarket.purchase_history (
+	cliente_id_history INT NOT NULL,
+	first_purchase DATE DEFAULT (CURRENT_DATE) NOT NULL,
+	last_purchase DATE DEFAULT (CURRENT_DATE) NOT NULL,
+	total_orders INT DEFAULT 1 NOT NULL,
+	total_spent DECIMAL(10,2) DEFAULT 0.00 NOT NULL,
+	avg_order_value DECIMAL(10,2) DEFAULT 0.00 NOT NULL,
+	crm_status ENUM('CLIENTE_NOVO', 'RECORRENTE', 'EM_RISCO', 'CHURN') DEFAULT 'CLIENTE_NOVO' NOT NULL,
+	CONSTRAINT purchase_history_pk PRIMARY KEY (cliente_id_history),
+	CONSTRAINT purchase_history_clients_FK FOREIGN KEY (cliente_id_history) REFERENCES novamarket.clients(client_id) ON DELETE CASCADE ON UPDATE CASCADE
 )
 ENGINE=InnoDB
 DEFAULT CHARSET=utf8mb4
